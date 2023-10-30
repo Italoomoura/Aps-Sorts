680a8b22354e58fb0e2397c351259b970aee156d
Italoomoura <italomourasant9@gmail.com>
2023-10-30 15:21:20 -0300

    comentários explicando cada parte da ordenação

--- aps/src/MergeSort.java
+++ aps/src/MergeSort.java
@@ -1,11 +1,16 @@
 public class MergeSort {
     public static void mergeSort(int[] arr, int left, int right) {
         if (left < right) {
+        	// Encontra o ponto médio do array
             int middle = (left + right) / 2;
-
+            
+            // Classifica a metade esquerda
             mergeSort(arr, left, middle);
+            
+            // Classifica a metade direita
             mergeSort(arr, middle + 1, right);
-
+            
+            // Combina as duas metades classificadas
             merge(arr, left, middle, right);
         }
     }
@@ -13,7 +18,8 @@ public class MergeSort {
     public static void merge(int[] arr, int left, int middle, int right) {
         int n1 = middle - left + 1;
         int n2 = right - middle;
-
+        
+        // Cria 2 arrays temporários para as duas metades
         int[] leftArray = new int[n1];
         int[] rightArray = new int[n2];
 
@@ -25,6 +31,8 @@ public class MergeSort {
         }
 
         int i = 0, j = 0, k = left;
+        
+        // Combina os dois arrays temporários em um único array ordenado
         while (i < n1 && j < n2) {
             if (leftArray[i] <= rightArray[j]) {
                 arr[k] = leftArray[i];
@@ -35,13 +43,15 @@ public class MergeSort {
             }
             k++;
         }
-
+        
+        // Copia quaisquer elementos restantes da metade esquerda
         while (i < n1) {
             arr[k] = leftArray[i];
             i++;
             k++;
         }
 
+        // Copia quaisquer elementos restantes da metade direita
         while (j < n2) {
             arr[k] = rightArray[j];
             j++;
--- aps/src/QuickSort.java
+++ aps/src/QuickSort.java
@@ -1,27 +1,40 @@
 public class QuickSort {
     public static void quickSort(int[] arr, int low, int high) {
         if (low < high) {
+        	// Encontra o pivô
             int pivotIndex = partition(arr, low, high);
+            
+            // Ordena a metade esquerda
             quickSort(arr, low, pivotIndex - 1);
+            
+            // Ordena a metade direita
             quickSort(arr, pivotIndex + 1, high);
         }
     }
 
     public static int partition(int[] arr, int low, int high) {
+    	
+    	// Escolhe o elemento mais à direita como pivô
         int pivot = arr[high];
+        
         int i = low - 1;
 
         for (int j = low; j < high; j++) {
             if (arr[j] < pivot) {
                 i++;
+                
+                // Troca elementos menores que o pivô para a esquerda
                 swap(arr, i, j);
             }
         }
-
+        
+        // Coloca o pivô em sua posição final
         swap(arr, i + 1, high);
+        
         return i + 1;
     }
-
+    
+    // Função para trocar elementos no array
     public static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
