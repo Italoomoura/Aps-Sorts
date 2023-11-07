3997bf006247284645af751fa4a1d686056eb29c
Italoomoura <italomourasant9@gmail.com>
2023-11-07 16:41:09 -0300

    Adicionado gráfico para o desempenho dos algoritmos de ordenação
    Modifiquei o código para incluir um gráfico de barras que exibe o
    desempenho de três algoritmos de ordenação (Merge Sort, Quick Sort e
    Bubble Sort) em relação ao número de itens na coluna do MySQL. O gráfico
    visualiza os tempos de execução desses algoritmos para tamanhos de
    dados, proporcionando uma comparação clara de sua eficiência. A
    biblioteca JFreeChart foi integrada para habilitar essa funcionalidade.
    Essa alteração melhora a usabilidade da ferramenta e facilita a seleção
    de algoritmos com base no tamanho dos dados.

--- aps/src/Launcher.java
+++ aps/src/Launcher.java
@@ -3,6 +3,12 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Arrays;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import javax.swing.*;
 
 public class Launcher {
 
@@ -13,8 +19,9 @@ public class Launcher {
         String password = "root";
 
         try (Connection connection = DriverManager.getConnection(jdbcUrl, username, password)) {
+        	String q = "SELECT * FROM dados limit 0, 5000";
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM dados");
+            ResultSet resultSet = statement.executeQuery(q);
             
             int count = 0;
             while (resultSet.next()) {
@@ -23,14 +30,16 @@ public class Launcher {
             String[] data = new String[count];
 
             // Recuperar os dados da tabela e armazená-los em um array
-            resultSet = statement.executeQuery("SELECT * FROM dados");
+            resultSet = statement.executeQuery(q);
             int index = 0;
             while (resultSet.next()) {
-                data[index] = resultSet.getString("tempo");
+                data[index] = resultSet.getString("nome");
                 index++;
             }
 
             // Agora você tem os dados da tabela no array 'data'
+            
+            DefaultCategoryDataset dataset = new DefaultCategoryDataset();
 
             // Aplicar o algoritmo de ordenação e medir o tempo para cada um deles
             String[] dataCopy = Arrays.copyOf(data, data.length); // Copiar os dados para manter o original inalterado
@@ -39,6 +48,8 @@ public class Launcher {
             long startTime = System.nanoTime();
             MergeSort.mergeSort(dataCopy, 0, dataCopy.length - 1);
             long mergeSortTime = (System.nanoTime() - startTime);
+            
+            dataset.addValue((mergeSortTime/1000000), "Merge Sort", String.valueOf(data.length));
 
             System.out.println("Merge Sort:");
             //MergeSort.printArray(dataCopy);
@@ -50,6 +61,8 @@ public class Launcher {
             startTime = System.nanoTime();
             QuickSort.quickSort(dataCopy, 0, dataCopy.length - 1);
             long quickSortTime = (System.nanoTime() - startTime);
+            
+            dataset.addValue((quickSortTime/1000000), "Quick Sort", String.valueOf(data.length));
 
             System.out.println("\nQuick Sort:");
             //QuickSort.printArray(dataCopy);
@@ -62,6 +75,25 @@ public class Launcher {
             BubbleSort bubbleSort = new BubbleSort();
             bubbleSort.bubble_sort(dataCopy);
             long bubbleSortTime = (System.nanoTime() - startTime);
+            
+            dataset.addValue((bubbleSortTime/1000000), "Bubble Sort", String.valueOf(data.length));
+            
+            JFreeChart chart = ChartFactory.createBarChart(
+                    "Tempo de Execução dos Algoritmos de Ordenação",
+                    "Quantidade de Itens",
+                    "Tempo (ms)",
+                    dataset,
+                    PlotOrientation.VERTICAL,
+                    true,
+                    true,
+                    false
+            );
+            
+            JFrame frame = new JFrame("Gráfico de Ordenação");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.getContentPane().add(new ChartPanel(chart));
+            frame.pack();
+            frame.setVisible(true);
 
             System.out.println("\nBubble Sort:");
             //bubbleSort.printArray(dataCopy);
